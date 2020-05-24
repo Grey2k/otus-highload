16e761de3a731fad5e12d7705148121645a88b33
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-05-24 18:15:23 +0300

    Добавление модели Друзья

--- app/database/models.py
+++ app/database/models.py
@@ -26,3 +26,19 @@ class Profile:
     gender: str
     city_id: int
     user_id: int
+
+
+@dataclass
+class Friendship:
+    id: int
+    source_id: int
+    destination_id: int
+    status: int
+    created_at: Date
+
+    @property
+    def confirmed(self):
+        return self.status == 1
+
+    def wait_confirmation(self, profile_id):
+        return self.destination_id == profile_id and self.status == 0
