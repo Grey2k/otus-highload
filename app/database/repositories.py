25a88dbe28b34319627e55619202ac9340751072
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-05-24 18:17:43 +0300

    Добавление функционала в репозитории

--- app/database/repositories.py
+++ app/database/repositories.py
@@ -1,5 +1,25 @@
+from dataclasses import dataclass
+from math import ceil
+
 from app.database.db import db
-from app.database.models import User, City, Profile
+from app.database.models import User, City, Profile, Friendship
+
+
+@dataclass
+class Pagination:
+    current_page: int
+    items_per_page: int
+    total_items: int
+
+    @property
+    def total_pages(self):
+        return ceil(self.total_items / self.items_per_page)
+
+
+@dataclass
+class PaginatedCollection:
+    pagination: Pagination
+    items: list
 
 
 class UserRepo:
@@ -43,8 +63,16 @@ class ProfileRepo:
 
         return profile
 
-    def find_friends(self, profile_id: int):
-        pass
+    @classmethod
+    def find_by_user_id(cls, user_id: int):
+        query = f'SELECT * from `{cls.table_name}` WHERE user_id=%s'
+        with db.cursor() as cursor:
+            cursor.execute(query, [user_id])
+            if cursor.rowcount == 0:
+                return None
+            profile = Profile(**cursor.fetchone())
+
+        return profile
 
     @classmethod
     def find_all(cls):
@@ -54,6 +82,17 @@ class ProfileRepo:
             profiles = [Profile(**row) for row in cursor.fetchall()]
 
         return profiles
+
+    @classmethod
+    def find_paginate(cls, page=1, count=10):
+        query = f'SELECT * from `{cls.table_name}` limit %s, %s'
+        count_query = f'SELECT * from `{cls.table_name}`'
+        with db.cursor() as cursor:
+            cursor.execute(query, ((page - 1) * count, count))
+            profiles = [Profile(**row) for row in cursor.fetchall()]
+            pagination = Pagination(current_page=page, items_per_page=count, total_items=cursor.execute(count_query))
+
+        return PaginatedCollection(items=profiles, pagination=pagination)
 
     @classmethod
     def create(cls, first_name, last_name, interests, birth_date, gender, city_id, user_id) -> Profile:
@@ -84,3 +123,89 @@ class CityRepo:
             cities = [City(**row) for row in cursor.fetchall()]
 
         return cities
+
+    @classmethod
+    def add(cls, name):
+        db.query('INSERT INTO `cities` (`name`) VALUES (%s)', name)
+        db.commit()
+
+    @classmethod
+    def find_by_id(cls, entity_id: int):
+        query = f'SELECT * from `{cls.table_name}` WHERE id=%s'
+        with db.cursor() as cursor:
+            cursor.execute(query, [entity_id])
+            if cursor.rowcount == 0:
+                return None
+            city = City(**cursor.fetchone())
+
+        return city
+
+    @classmethod
+    def find_by_ids(cls, ids):
+        if not ids:
+            return []
+        query = f'SELECT * from `{cls.table_name}` WHERE id in %s'
+        with db.cursor() as cursor:
+            cursor.execute(query, [set(ids)])
+            cities = {row['id']: City(**row) for row in cursor.fetchall()}
+
+        return cities
+
+
+class FriendRepo:
+    table_name = 'friends'
+    profile_table_name = 'profiles'
+
+    @classmethod
+    def find_all(cls, profile_id):
+        query = f'''
+        SELECT * from `{cls.profile_table_name}`
+            WHERE id in (
+                SELECT destination_id from `{cls.table_name}` WHERE source_id = %(profile_id)s and status = %(status)s
+                UNION
+                SELECT source_id from `{cls.table_name}` WHERE destination_id = %(profile_id)s and status = %(status)s
+            )'''
+        with db.cursor() as cursor:
+            cursor.execute(query, {'profile_id': profile_id, 'status': 1})
+            profiles = [Profile(**row) for row in cursor.fetchall()]
+
+        return profiles
+
+    @classmethod
+    def find_incoming_requests(cls, profile_id):
+        query = f'''
+            SELECT p.* from `{cls.table_name}` as f
+            LEFT JOIN `{cls.profile_table_name}` as p on p.id = f.source_id
+            WHERE f.destination_id = %s and f.status = 0
+        '''
+        with db.cursor() as cursor:
+            cursor.execute(query, [profile_id])
+            profiles = [Profile(**row) for row in cursor.fetchall()]
+
+        return profiles
+
+    @classmethod
+    def find_outgoing_requests(cls, profile_id):
+        query = f'''
+           SELECT p.* from `{cls.table_name}` as f
+           LEFT JOIN `{cls.profile_table_name}` as p on p.id = f.destination_id
+           WHERE f.source_id = %s and f.status = 0
+        '''
+        with db.cursor() as cursor:
+            cursor.execute(query, [profile_id])
+            profiles = [Profile(**row) for row in cursor.fetchall()]
+
+        return profiles
+
+    @classmethod
+    def find_friendship(cls, profile_one, profile_two):
+        query = f'''
+           SELECT * from `{cls.table_name}`
+           WHERE (source_id = %(one)s and destination_id = %(two)s) 
+              or (source_id = %(two)s and destination_id = %(one)s)
+        '''
+        with db.cursor() as cursor:
+            cursor.execute(query, {'one': profile_one, 'two': profile_two})
+            if cursor.rowcount == 0:
+                return None
+            return Friendship(**cursor.fetchone())
