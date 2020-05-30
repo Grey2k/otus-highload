6fda391731991c3b26baccf5124db609dbd23ccb
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-05-30 13:53:52 +0300

    Декоратор для включения транзакции

--- app/database/db.py
+++ app/database/db.py
@@ -1,3 +1,22 @@
+import functools
+
+from pymysql import MySQLError
+
 from app.ext.mysql import Mysql
 
 db = Mysql()
+
+
+def transactional(fn):
+    @functools.wraps(fn)
+    def wrapped(*args, **kwargs):
+        try:
+            db.begin_transaction()
+            result = fn(*args, **kwargs)
+            db.commit()
+            return result
+        except MySQLError as e:
+            db.rollback()
+            raise e
+
+    return wrapped
