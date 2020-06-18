9d1d27c900f5191d121131324408c9331d0f35a7
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-06-18 22:30:07 +0300

    Изменение индекса

--- app/migrations/1589046132_profiles_index.py
+++ app/migrations/1589046132_profiles_index.py
@@ -1,6 +1,6 @@
 
 def up(db):
-    db.query('CREATE FULLTEXT INDEX ln_fn_idx on profiles(last_name, first_name)')
+    db.query('CREATE FULLTEXT INDEX ln_fn_idx on profiles(last_name)')
 
 
 def down(db):
