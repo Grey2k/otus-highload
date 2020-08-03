f3954fddd783c6a66f027b6e573a43e959b4ee4c
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-08-03 11:00:27 +0300

    Регистрация DialogsRepo

--- app/di.py
+++ app/di.py
@@ -2,7 +2,7 @@ from flask import current_app
 from injector import singleton
 
 from app.database.db import pool
-from app.database.repositories import CityRepo, UserRepo, ProfileRepo, FriendRepo
+from app.database.repositories import CityRepo, UserRepo, ProfileRepo, FriendRepo, DialogsRepo
 
 
 def configure_di(binder):
@@ -10,6 +10,7 @@ def configure_di(binder):
     binder.bind(UserRepo, to=UserRepo(pool), scope=singleton)
     binder.bind(ProfileRepo, to=ProfileRepo(pool), scope=singleton)
     binder.bind(FriendRepo, to=FriendRepo(pool), scope=singleton)
+    binder.bind(DialogsRepo, to=DialogsRepo(pool), scope=singleton)
 
 
 def get(service):
