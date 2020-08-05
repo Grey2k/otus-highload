5fb2724840843279d077e1b14fe6f29360483945
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-08-05 11:40:19 +0300

    Исправление миграции создания таблиц диалогов

--- app/migrations/1596439594_create_dialogs.py
+++ app/migrations/1596439594_create_dialogs.py
@@ -19,11 +19,15 @@ def up(db):
         CREATE TABLE IF NOT EXISTS dialogs_messages
             (
                 id bigint auto_increment,
+                dialog_id bigint not null,
                 sender_id bigint not null,
                 created_at datetime not null default NOW(),
                 text text,
                 constraint dialogs_messages_pk
                     primary key (id),
+                constraint dialogs_messages_dialogs_dialog_id_fk
+                    foreign key (dialog_id) references dialogs (id)
+                        on delete cascade,
                 constraint dialogs_messages_profiles_sender_id_fk
                     foreign key (sender_id) references profiles (id)
                         on delete cascade
@@ -33,12 +37,15 @@ def up(db):
     sql = f'''
         CREATE TABLE IF NOT EXISTS dialogs_participants
             (
-                id bigint auto_increment,
+                dialog_id bigint not null,
                 profile_id bigint not null,
                 constraint dialogs_participants_pk
-                    primary key (id),
-                constraint dialogs_participants_profiles_sender_id_fk
+                    primary key (dialog_id, profile_id),
+                constraint dialogs_participants_profiles_profile_id_fk
                     foreign key (profile_id) references profiles (id)
+                        on delete cascade,
+                constraint dialogs_participants_dialog_dialog_id_fk
+                    foreign key (dialog_id) references dialogs (id)
                         on delete cascade
             )
         '''
