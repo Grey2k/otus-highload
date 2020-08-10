fb7366ab3c49b62ed1ddc8db80362df2ed8ac565
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-08-10 23:04:18 +0300

    Поиск из тарантула функция на lua

--- conf/tarantool/app.lua
+++ conf/tarantool/app.lua
@@ -1,3 +1,19 @@
 function count_profiles()
     return box.space.soc_net_profiles:count()
 end
+
+
+function search_profiles(first_name, last_name, page, cnt)
+
+    local cnt_sql = string.format(
+        'SELECT count(*) FROM "soc_net_profiles" WHERE "first_name" LIKE \'%s\' AND "last_name" LIKE \'%s\'',
+         first_name, last_name
+    )
+
+    local sql = string.format(
+        'SELECT * FROM "soc_net_profiles" WHERE "first_name" LIKE \'%s\' AND "last_name" LIKE \'%s\' LIMIT %d, %d',
+         first_name, last_name, (page - 1) * cnt, cnt
+    )
+
+    return {cnt=box.execute(cnt_sql).rows[1][1], items=box.execute(sql).rows}
+end
