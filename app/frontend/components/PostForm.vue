bb96971ac077c27ef807a928ea0482c58689fa5a
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-08-20 22:00:41 +0300

    Лента новостей. Форма создания поста в popup

--- app/frontend/components/PostForm.vue
+++ app/frontend/components/PostForm.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="mx-3 my-4 text-right">
-    <b-form-textarea
-        :state="isValid"
-        rows="3"
-        class="mb-3"
-        placeholder="Tell something"
-        v-model="content"
-    ></b-form-textarea>
-    <b-button variant="primary" v-if="true" v-on:click="createPost">
-      Publish
+  <div>
+    <b-button class="create-post" variant="primary" v-b-modal.create-post-modal>
+      <b-icon icon="plus-circle" font-scale="2"></b-icon>
     </b-button>
+    <b-modal
+        id="create-post-modal"
+        title="New post"
+        ok-only
+        ok-title="Publish"
+        @ok="createPost"
+    >
+      <b-form-textarea
+          :state="isValid"
+          rows="10"
+          class="mb-3"
+          placeholder="Tell something"
+          v-model="content"
+      ></b-form-textarea>
+    </b-modal>
   </div>
 </template>
 
@@ -26,9 +34,12 @@ export default {
     }
   },
   methods: {
-    createPost: async function () {
+    createPost: async function (bvModalEvt) {
+      bvModalEvt.preventDefault()
+
       const formData = new FormData()
       formData.append('content', this.content)
+
 
       const response = await fetch(this.submitUrl, {
         'method': 'POST',
@@ -36,11 +47,29 @@ export default {
       })
 
       const data = await response.json()
-      this.isValid = data.success ? null : false;
+      this.isValid = data.success ? null : false
+      if (!data.success) {
+        return
+      }
       if (data.redirect) {
         location.href = data.redirect
       }
+      this.$nextTick(() => {
+        this.$bvModal.hide('create-post-modal')
+      })
     }
   }
 }
 </script>
+<style>
+.create-post {
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  z-index: 99;
+  padding: 0;
+}
+</style>
