51b004977b1a843a2eef54ba9327ab7e6715063c
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-08-23 17:19:56 +0300

    Лента новостей. Работа фоновых задач в контексте приложения

--- app/app.py
+++ app/app.py
@@ -1,7 +1,17 @@
 import os
 
-from app import create_app, celery as flask_celery
+from app import create_app
+from app.celery import celery as flask_celery
 
 app = create_app(env=os.environ.get('ENV'))
 
 celery = flask_celery.celery
+
+
+class ContextTask(celery.Task):
+    def __call__(self, *args, **kwargs):
+        with app.app_context():
+            return self.run(*args, **kwargs)
+
+
+celery.Task = ContextTask
